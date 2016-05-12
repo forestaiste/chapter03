7b06d6260a62b8aea6c04c2e4d6142b4213d32cb
forestaiste <forestaiste@hotmail.com>
2016-05-12 16:36:40 +0800

    Initial commit

---
+++ src/main/com/forest/chapter03/form/ProductForm.java
@@ -0,0 +1,29 @@
+package com.forest.chapter03.form;
+
+/**
+ * Created by A21962 on 5/9/2016.
+ */
+public class ProductForm {
+    private String name;
+    private String description;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
